b51a250e70407e0af4d13c74264b255d462273b7
RITESH DILKUSH CHARI <144829970+RITESH05-rdc@users.noreply.github.com>
2025-12-23 00:52:16 +0530

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def load_models():
 
     diarization_pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
-        use_auth_token=hf_token
+        token=hf_token
     )
 
     whisper_model = whisper.load_model("tiny")  # deployment-safe
@@ -164,3 +164,4 @@ if uploaded_file:
 
     if os.path.exists(audio_path):
         os.remove(audio_path)
+
